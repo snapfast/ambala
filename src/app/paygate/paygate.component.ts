fa518527be62f190ffc9a7f317edf131a3a7215b
Rahul Bali <rahulrdb18@gmail.com>
2024-05-19 14:33:17 +0530

    some

--- src/app/paygate/paygate.component.ts
+++ src/app/paygate/paygate.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-paygate',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './paygate.component.html',
   styleUrl: './paygate.component.css'
 })
 export class PaygateComponent {
+
+  // redirect to external webstie after clicking a button in new tab
+  redirectToExternalWebsite() {
+    window.open('https://www.google.com', '_blank');
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
