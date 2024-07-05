30b3fa3e8c16bc319d6347899045cd72d68a7ac0
Rahul Bali <rahulrdb18@gmail.com>
2024-07-06 05:11:40 +0530

    Adding Main Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ import { PaygateComponent } from './paygate/paygate.component';
 
 export const routes: Routes = [
     { path: '', component: AppointmentComponent },
+    { path: 'main', component: MainComponent },
     { path: 'paygate', component: PaygateComponent },
 ];
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common'
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+// import material web button 3
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NgOptimizedImage,
+    RouterModule,
+    MatButtonModule
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+  title = 'MAIN';
+
+  constructor(private router: Router) { }
+
+  // This function to navigate to another component
+  navigateToAppointment() {
+    this.router.navigate(['/paygate']);
+  }
+
+}
+
